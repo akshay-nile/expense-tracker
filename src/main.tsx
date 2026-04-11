070559b8e3906483f47e4aab13d98ab97ecf0866
Akshay Nile <akshaynile8849@gmail.com>
2026-04-11 23:21:46 +0530

    removed the service-worker.js

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { PrimeReactProvider } from 'primereact/api';
-import { confirmDialog } from 'primereact/confirmdialog';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import type { Theme } from './services/models.ts';
 import App from './App.tsx';
 import './index.css';
+import type { Theme } from './services/models.ts';
 
 const THEME_KEY = 'expense-tracker-theme';
 if (THEME_KEY in localStorage) setAppTheme(localStorage.getItem(THEME_KEY) as Theme);
@@ -25,31 +24,3 @@ createRoot(document.getElementById('root')!).render(
     </PrimeReactProvider>
   </StrictMode>
 );
-
-if ('serviceWorker' in navigator && window.location.href.startsWith('https')) {
-  window.addEventListener('load', async () => {
-    try {
-      const registration = await navigator.serviceWorker.register('./service-worker.js');
-      await navigator.serviceWorker.ready;
-      console.log('Service Worker Registered!');
-
-      navigator.serviceWorker.addEventListener('message', (event) => {
-        if (event.data === 'UPDATED') {
-          confirmDialog({
-            header: 'New Update Installed',
-            message: 'Reload the app now?',
-            icon: 'pi pi-sync',
-            closable: false,
-            accept: () => window.location.reload()
-          });
-          return;
-        }
-        console.log('Message From SW:', event.data);
-      });
-
-      const controller = navigator.serviceWorker.controller ?? registration.active;
-      controller?.postMessage('CHECK-UPDATE');
-    }
-    catch (err) { console.error('Service Worker Error:', err); }
-  });
-}
